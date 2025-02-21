277d44ae6e587023e6aca0234871fc2d1a380839
DanielBarletta81 <dbarcelona1481@gmail.com>
2025-02-20 23:11:27 -0500

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieList from './components/MovieList'
+
+function App() {
+  return (
+    <div>
+      <h1>My Favorite Movies!</h1>
+      <MovieList />
+    </div>
+  )
+}
+
+export default App
+
